861da3128b77bea7f55fa4b750e8ad3329247145
Hound <eddie.ch.89d@gmail.com>
2021-02-07 09:00:01 -0500

    Fibonacci

--- fibo.py
+++ fibo.py
@@ -7,8 +7,6 @@ from modules import plot_fibo_bar_values as pv
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
-from math import e
-from math import pi
 
 
 
@@ -28,47 +26,6 @@ def display_sequence(num_arr):
 
 
 
-def ebonanacci_plot(labels, fibo_out):
-
-    tmp_arr = []
-    tmp_arr = fibo_out
-    counter_e = 0
-
-    for x in fibo_out:
-        fibo_out[counter_e] = fibo_out[counter_e] + (1-e**(-fibo_out[counter_e] + pi) + pi)
-        counter_e += 1
-
-	
-    x = np.arange(len(labels))
-    width = 0.35
-
-    fig, ax = plt.subplots()
-    rects1 = ax.bar(x - width/10, fibo_out, width*2, label="Ebonanacci Sequence", color="red")
-
-    ax.set_ylabel('Ebonacci Output')
-    ax.set_xticks(x)
-    ax.set_xticklabels(labels)
-    ax.legend()
-
-
-    def autolabel(rects):
-            for rect in rects:
-                height = rect.get_height()
-                ax.annotate('{}'.format(height),
-                            xy=(rect.get_x() + rect.get_width() / 2, height),
-                            xytext=(0, 3),  # 3 points vertical offset
-                            textcoords="offset points",
-                            color="black",
-                            ha='center', va='bottom')
-
-    plt.xlabel("Value")
-    autolabel(rects1)
-    fig.tight_layout()
-    plt.show()
-
-
-
-
 def main():
 	nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 	fibo_out = []
@@ -78,7 +35,7 @@ def main():
 	
 	plt_values = pv.Plot_values(nums, fibo_out)
 	plt_values.plot_fibo_values()
-	ebonanacci_plot(nums, fibo_out)
+	plt_values.ebonanacci_plot()
 
 
 
--- modules/plot_fibo_bar_values.py
+++ modules/plot_fibo_bar_values.py
@@ -1,7 +1,9 @@
-
+#!/usr/bin/env python3
 import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
+from math import e
+from math import pi
 
 
 
@@ -10,6 +12,7 @@ class Plot_values:
 	def __init__(self, labels, fibo_out):
 		self.labels = labels
 		self.fibo_out = fibo_out
+
 
 
 	def plot_fibo_values(self):
@@ -38,3 +41,41 @@ class Plot_values:
 		autolabel(rects1)
 		fig.tight_layout()
 		plt.show()
+
+
+
+	def ebonanacci_plot(self):
+		tmp_arr = []
+		tmp_arr = self.fibo_out
+		counter_e = 0
+
+		for x in self.fibo_out:
+		    self.fibo_out[counter_e] = self.fibo_out[counter_e] + (1-e**(-self.fibo_out[counter_e] + pi) + pi)
+		    counter_e += 1
+
+		x = np.arange(len(self.labels))
+		width = 0.35
+
+		fig, ax = plt.subplots()
+		rects1 = ax.bar(x - width/10, self.fibo_out, width*2, label="Ebonanacci Sequence", color="red")
+
+		ax.set_ylabel('Ebonacci Output')
+		ax.set_xticks(x)
+		ax.set_xticklabels(self.labels)
+		ax.legend()
+
+		def autolabel(rects):
+		        for rect in rects:
+		            height = rect.get_height()
+		            ax.annotate('{}'.format(height),
+		                        xy=(rect.get_x() + rect.get_width() / 2, height),
+		                        xytext=(0, 3),  # 3 points vertical offset
+		                        textcoords="offset points",
+		                        color="black",
+		                        ha='center', va='bottom')
+
+		plt.xlabel("Value")
+		autolabel(rects1)
+		fig.tight_layout()
+		plt.show()
+
